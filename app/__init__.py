3ae4ffe1b78aecdd2e971ea049643c3e38f93340
Tara Can <taracan@Taras-MacBook-Air.local>
2025-08-09 20:24:34 -0500

    add sqlite db

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 import os
+from flask_sqlalchemy import SQLAlchemy 
 
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
@@ -9,6 +11,9 @@ def create_app():
     app.secret_key = os.environ.get("SECRET_KEY", secret_key)
 
     app.config['SECRET_KEY'] = secret_key
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+    db.init_app(app)
 
     from . import routes
     app.register_blueprint(routes.bp)
